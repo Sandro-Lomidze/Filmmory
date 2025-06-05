180159eada502bdc0f6ce02b47be8f2ca8d1f6f4
Sandro-Lomidze <sandro.lomidze.1@btu.edu.ge>
2025-06-05 17:48:56 +0400

    v0.4
    - Date widgets now get disabled based on selected status.
    - Disabled date widgets return "-".
    - Tweaked the Anti-Duplicate system.
    - Import button no longer changes text on startup.

--- Filmmory.py
+++ Filmmory.py
@@ -41,16 +41,35 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.titles = set()
 
         #This function includes setting headers and their sizes, since .clear() removes them too.
-        self.import_movies_interactive()
+        self.import_movies()
 
         self.unknown_start_date.stateChanged.connect(self.unknown_start_date_checked)
         self.unknown_finish_date.stateChanged.connect(self.unknown_finish_date_checked)
+        self.status_combo.currentTextChanged.connect(self.status_date_control)
 
         self.add_button.clicked.connect(self.add_movie)
         self.remove_button.clicked.connect(self.remove_movie)
         self.import_button.clicked.connect(self.import_movies_interactive)
         self.save_button.clicked.connect(self.save_movies_interactive)
         self.sort_button.clicked.connect(self.sorter)
+
+
+    # Disables Date Widgets based on selected status.
+    def status_date_control(self):
+        if self.status_combo.currentIndex() == 1:
+            self.start_date.setEnabled(True)
+            self.finish_date.setEnabled(True)
+            self.unknown_start_date.setEnabled(True)
+            self.unknown_finish_date.setEnabled(True)
+        elif self.status_combo.currentIndex() == 0:
+            self.start_date.setEnabled(True)
+            self.finish_date.setEnabled(False)
+            self.unknown_finish_date.setEnabled(False)
+        else:
+            self.start_date.setEnabled(False)
+            self.finish_date.setEnabled(False)
+            self.unknown_start_date.setEnabled(False)
+            self.unknown_finish_date.setEnabled(False)
 
 
     # To disable QDate widgets when checking boxes.
@@ -70,13 +89,17 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     # To return date values based on boxes checked.
     # Last line changes PyQt5.QtCore.QDate(2025, 6, 2) -> "2/6/2025".
     def start_date_value(self):
-        if self.unknown_start_date.isChecked():
+        if self.status_combo.currentIndex() == 2:
+            return '-'
+        elif self.unknown_start_date.isChecked():
             return '?'
         else:
             return '/'.join(str(self.start_date.date())[19:29].split(', ')[::-1])
 
     def finish_date_value(self):
-        if self.unknown_finish_date.isChecked():
+        if self.status_combo.currentIndex() == 0 or self.status_combo.currentIndex() == 2:
+            return '-'
+        elif self.unknown_finish_date.isChecked():
             return '?'
         else:
             return '/'.join(str(self.finish_date.date())[19:29].split(', ')[::-1])
@@ -95,7 +118,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def add_movie(self):
         if self.title.text() == '':
             pass
-        elif self.title.text() in self.titles:
+        elif self.title.text().lower() in self.titles:
             self.add_button.setText('Already Present!')
             QTimer.singleShot(1000, lambda: self.add_button.setText('Add'))
         else:
@@ -105,9 +128,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 QStandardItem(self.score_value()),
                 QStandardItem(self.start_date_value()),
                 QStandardItem(self.finish_date_value())])
-            self.titles.add(self.title.text())
+            self.titles.add(self.title.text().lower())
             self.title.clear()
-
 
 
     # Finds selected row indexes and removes them decreasingly
@@ -117,12 +139,12 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         selections = self.movie_table_view.selectionModel().selectedRows()
         if not selections:
             last_row = self.movie_table_model.rowCount() - 1
-            self.titles.discard(self.movie_table_model.item(last_row, 0).text())
+            self.titles.discard(self.movie_table_model.item(last_row, 0).text().lower())
             self.movie_table_model.removeRow(last_row)
         else:
             selection_indexes = reversed(sorted(index.row() for index in selections))
             for each in selection_indexes:
-                self.titles.discard(self.movie_table_model.item(each, 0).text())
+                self.titles.discard(self.movie_table_model.item(each, 0).text().lower())
                 self.movie_table_model.removeRow(each)
 
 
@@ -137,7 +159,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             for line in filmmory_save1:
                 tags = line.strip('\n').split(' | ')
                 self.titles = set()
-                self.titles.add(tags[0])
+                self.titles.add(tags[0].lower())
                 single_movie = Movie(tags)
                 self.movie_table_model.appendRow([
                     QStandardItem(single_movie.title),
